ec748304eb650a71c941377624c3318fd7c8dfe5
John <duanjohn99@gmail.com>
2025-04-01 16:17:01 -0400

    docker containers working, need to add database orms and then test the aggregation scripts

--- src/main.py
+++ src/main.py
@@ -1,9 +1,18 @@
 import logging
-
+import time
 
 
 def main():
     logging.basicConfig(level = logging.INFO)
-    logging.info("flag hi")
+    logging.info("starting main.py")
+
+    # connect to the database
+    
+    # run scraping scripts
+
+
+
+if __name__ == "__main__":
+    main()
 
     
