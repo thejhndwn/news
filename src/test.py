0c284c0da95f949be366ce90d1ca21608f7d5fa7
John <duanjohn99@gmail.com>
2025-07-18 18:14:14 -0400

    removing artifacts

--- src/test.py
+++
@@ -1,7 +0,0 @@
-# from huggingface_hub import login
-from production.AudioGenerator import AudioGenerator
-
-
-AudioGenerator = AudioGenerator("/home/john-duan/Celia/news/output")
-AudioGenerator.generate(11111)
-print("done")
